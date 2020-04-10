611a15d4902fd53629b6d48548ff022652294fd9
Praveen Rajput <praveenislive@gmail.com>
2020-04-11 01:37:09 +0530

    Update Converter.swift

--- Numero/Converter.swift
+++ Numero/Converter.swift
@@ -33,7 +33,7 @@ class Converter {
     var result = ""
 
     let numberSymbols: [(number: Int, symbol: String)] =
-      [(1000, "A"),
+      [(1000, "Z"),
        (900, "CM"),
        (500, "D"),
        (400, "CD"),
